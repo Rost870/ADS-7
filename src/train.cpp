099d089a48409b921ef6a610a30e5dedcaa28391
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-05-19 12:39:58 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,46 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2025 NNTU-CS
 #include "train.h"
+
+Train::Train() : countOp(0), first(nullptr) {}
+
+Train::~Train() {
+  if (!first) return;
+  Car* cur = first->next;
+  while (cur != first) {
+    Car* tmp = cur->next;
+    delete cur;
+    cur = tmp;
+  }
+  delete first;
+}
+
+void Train::addCar(bool light) {
+  Car* car = new Car{light, nullptr, nullptr};
+  if (!first) {
+    car->next = car;
+    car->prev = car;
+    first = car;
+  } else {
+    Car* last = first->prev;
+    last->next = car;
+    car->prev  = last;
+    car->next  = first;
+    first->prev = car;
+  }
+}
+int Train::getLength() {
+  if (!first) return 0;
+  countOp = 0;
+  int len = 0;
+  Car* cur = first;
+  do {
+    len++;
+    countOp++;
+    cur = cur->next;
+  } while (cur != first);
+  return len;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
