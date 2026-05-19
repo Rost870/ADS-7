d58c26ba5d2883a2f7dc56436146c4318b47a899
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-05-19 12:51:12 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,32 @@
 // Copyright 2025 NNTU-CS
 #include <iostream>
-#include <cstdlib>
 #include <ctime>
 #include "train.h"
 
 int main() {
-  srand(static_cast<unsigned>(time(nullptr)));
+  unsigned seed = static_cast<unsigned>(time(nullptr));
+
   std::cout << "n\tall_off\tall_on\trandom\n";
+
   for (int n = 2; n <= 100; n += 2) {
     Train t1;
     for (int i = 0; i < n; i++) t1.addCar(false);
     t1.getLength();
-    int ops_off = t1.getOpCount();
+
     Train t2;
     for (int i = 0; i < n; i++) t2.addCar(true);
     t2.getLength();
-    int ops_on = t2.getOpCount();
+
     Train t3;
-    for (int i = 0; i < n; i++) t3.addCar(rand() % 2 == 0);
+    for (int i = 0; i < n; i++) {
+      seed = seed * 1664525u + 1013904223u;  // LCG без rand()
+      t3.addCar((seed >> 16) & 1);
+    }
     t3.getLength();
-    int ops_rnd = t3.getOpCount();
-    std::cout << n << "\t" << ops_off << "\t"
-              << ops_on << "\t" << ops_rnd << "\n";
+
+    std::cout << n << "\t" << t1.getOpCount()
+              << "\t" << t2.getOpCount()
+              << "\t" << t3.getOpCount() << "\n";
   }
 
   return 0;
