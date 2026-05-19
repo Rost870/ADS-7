7334e9c90db6225faa71fc1434f57b438a3e2366
Rost870 <60008611+Rost870@users.noreply.github.com>
2026-05-19 12:55:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,23 @@
 
 int main() {
   unsigned seed = static_cast<unsigned>(time(nullptr));
-
   std::cout << "n\tall_off\tall_on\trandom\n";
-
   for (int n = 2; n <= 100; n += 2) {
     Train t1;
     for (int i = 0; i < n; i++) t1.addCar(false);
     t1.getLength();
-
     Train t2;
     for (int i = 0; i < n; i++) t2.addCar(true);
     t2.getLength();
-
     Train t3;
     for (int i = 0; i < n; i++) {
-      seed = seed * 1664525u + 1013904223u;  // LCG без rand()
+      seed = seed * 1664525u + 1013904223u;
       t3.addCar((seed >> 16) & 1);
     }
     t3.getLength();
-
     std::cout << n << "\t" << t1.getOpCount()
               << "\t" << t2.getOpCount()
               << "\t" << t3.getOpCount() << "\n";
   }
-
   return 0;
 }
